3caffa912e3d936791063dc1822ca8f2d0a8c59e
alexmitchell <alcomposer@gmail.com>
2021-07-29 19:09:15 +0930

    toto.diff applied from jpcima

--- fabricController.cpp
+++ fabricController.cpp
@@ -22,15 +22,19 @@ fabricController::fabricController(Widget *widget, Size<uint> size) noexcept
     : VolumeKnob(widget, size)
 {
     //setSize(size);
+
+    setColor(Color(173, 216, 230, 255));
+
     
-    flabelTop = new NanoLabel(widget, Size<uint>(2000,200));
+    flabelTop = new NanoLabel(widget, Size<uint>(200,200));
     flabelTop->setFontSize(20.0);
-    flabelTop->textAlign(ALIGN_CENTER);
+    //flabelTop->textAlign(ALIGN_CENTER);
+    flabelTop->setAlign(ALIGN_CENTER|ALIGN_TOP);
     flabelTop->show();
 
-    fknobCenter = new VolumeKnob(widget,  Size<uint>(60, 60));
-    fknobCenter->setColor(Color(173, 216, 230, 255));
-    fknobCenter->show();
+    // fknobCenter = new VolumeKnob(widget,  Size<uint>(60, 60));
+    // fknobCenter->setColor(Color(173, 216, 230, 255));
+    // fknobCenter->show();
 
     updateBuddyWidgetPositions();
 }
@@ -42,6 +46,7 @@ void fabricController::nanoKnobValueChanged(NanoKnob *nanoKnob, const float valu
 
 void fabricController::onNanoDisplay()
 {
+    VolumeKnob::onNanoDisplay();
 }
 
 void fabricController::setText(std::string text)
@@ -68,12 +73,20 @@ void fabricController::updateBuddyWidgetPositions()
 
     Rectangle<float> labelTopBounds;
     flabelTop->textBounds(0.0, 0.0, topText.c_str(), NULL, labelTopBounds);
-    std::cout << labelTopBounds.getWidth() << std::endl;
+    //std::cout << labelTopBounds.getWidth() << std::endl;
 
     // reposition the label at horizontal center and above the knob
-    flabelTop->setAbsolutePos(area.getX() - labelTopBounds.getWidth() / 2.0, area.getY() - labelTopBounds.getHeight() - 4);
+    //flabelTop->setAbsolutePos(area.getX() - labelTopBounds.getWidth() / 2.0, area.getY() - labelTopBounds.getHeight() - 4);
+
+    flabelTop->setAbsolutePos(
+        area.getX() + (area.getWidth() - flabelTop->getWidth()) / 2.0,
+        area.getY() - labelTopBounds.getHeight() - 4);
+
+// fprintf(stderr, "area %d %d\n", area.getX(), area.getWidth());
+// fprintf(stderr, "label %d %d\n", flabelTop->getAbsoluteX(), flabelTop->getWidth());
+
     // reposition the knob at top-left
-    fknobCenter->setAbsolutePos(area.getX(), area.getY());
+    //fknobCenter->setAbsolutePos(area.getX(), area.getY());
 }
 
 END_NAMESPACE_DISTRHO
