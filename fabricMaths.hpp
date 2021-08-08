1b0299166bc944b2d1531cf250991e476c17b7df
alexmitchell <alcomposer@gmail.com>
2021-08-08 23:45:03 +0930

    cleanup maths of window function

--- fabricMaths.hpp
+++ fabricMaths.hpp
@@ -8,8 +8,8 @@ namespace FABRICMATHS {
 
 inline float tukeyWindow(float x, float sides, float tilt)
 {   
-    float timeWarp = tilt < 0? (pow(x, 1 + tilt) / pow(1,1 + tilt)) : (pow(1- x, 1 - tilt) / pow(1,1 - tilt));
-    return (cos(fmax(fabs((double)timeWarp - 0.5) * (2.0 / sides)  - (1.0 / sides - 1.0), 0.0) * PI) + 1.0) / 2.0; 
+    float timeWarp = tilt < 0? pow(x, 1 + tilt) : pow(1 - x, 1 - tilt);
+    return (cos(fmax(fabs((double)timeWarp - 0.5) * (2.0 / sides)  - (1.0 / sides - 1.0), 0.0) * PI) + 1.0) * 0.5; 
 }
 
 }
